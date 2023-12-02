2b85af7d449bc84a8a36b116ae22102805087617
zft9xgy <zft9xgy@proton.me>
2023-12-02 13:14:40 +0100

    Updates

--- app/models.py
+++ app/models.py
@@ -20,17 +20,3 @@ class Idea(models.Model):
     def update_votes(self):
         self.votes = self.voters.count()
 
-    
-class App(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    creator = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True,null=True)
-    ideas = models.ForeignKey(Idea,on_delete=models.CASCADE,blank=True,null=True)
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-    
-    class Meta():
-        ordering = ['title']
